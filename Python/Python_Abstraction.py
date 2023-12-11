ddeb021d8b9ae2c1b504dc125e44f335a2892a2e
Fahad <ftkhan61814@gmail.com>
2023-12-11 20:53:45 +0530

    Abstraction

--- Python/Python_Abstraction.py
+++ Python/Python_Abstraction.py
@@ -1,1 +1,30 @@
-# oops abstraction 
+# Oops abstraction 
+# abstraction allows you to hide complex details and 
+# focus on the essential aspects of your code, 
+# making it more manageable and understandable, 
+# especially as your codebase grows.
+
+import abc
+class Shape:  # Abstract class
+    abc.abstractmethod
+    def area(self):
+        pass
+
+    abc.abstractmethod
+    def perimeter(self):
+        pass
+    
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+
+
+circle = Circle(5)
+print("Area:", circle.area())
+print("Perimeter:", circle.perimeter())
